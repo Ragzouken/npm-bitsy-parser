590955afc57d1948a6e4a62fa8913e14269bfba4
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-04-27 13:52:33 -0400

    fix issue with extra newline being printed after graphics (again)
    it's an issue with every frame, not just the last one

--- lib/index.ts
+++ lib/index.ts
@@ -102,7 +102,7 @@ export class BitsyObjectBase extends BitsyResourceBase implements BitsyObject
     {
         const props = [];
         props.push(super.toString());
-        props.push(this.graphic.map(g => g.map(b => b ? 1 : 0).join('').replace(/(.{8})/g, '$1\n')).join('\n>\n').trim());
+        props.push(this.graphic.map(g => g.map(b => b ? 1 : 0).join('').replace(/(.{8})/g, '$1\n').trim()).join('\n>\n'));
         if (this.name)
         {
             props.push(`NAME ${this.name}`);
