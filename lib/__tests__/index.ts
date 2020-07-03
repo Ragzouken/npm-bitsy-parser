d907f361fc959ee7d8cab8574621a95cfc746178
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-07-03 00:24:34 -0400

    add 2.2 tests + complex comments

--- lib/__tests__/index.ts
+++ lib/__tests__/index.ts
@@ -4,11 +4,13 @@ import { BitsyParser } from '..';
 
 test.each`
 	file         | version
+	${'default'} | ${'2.2'}
+	${'complex'} | ${'2.2' /*includes legacy walls */}
 	${'default'} | ${'4.6'}
 	${'default'} | ${'5.1'}
 	${'default'} | ${'6.2'}
 	${'default'} | ${'7.1'}
-	${'complex'} | ${'7.1'}
+	${'complex'} | ${'7.1' /*includes extended palette, extended animation frames, exit transitions, exit dialog */}
 `('parses and serializes $file sample for bitsy v$version without modifying it', ({ file, version }) => {
 	const gamedata = readFileSync(resolve(__dirname, `./${file}_${version}.txt`), { encoding: 'utf-8' });
 	expect(BitsyParser.parse(gamedata.split('\n')).toString()).toBe(gamedata);
