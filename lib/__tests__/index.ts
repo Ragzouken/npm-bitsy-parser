f8ea7ae47cb218083aecf1b1f74bbb0661322475
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-07-02 23:03:59 -0400

    test default files from multiple versions

--- lib/__tests__/index.ts
+++ lib/__tests__/index.ts
@@ -2,7 +2,13 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { BitsyParser } from '..';
 
-it('parses and serializes correctly formatted data without modifying it', () => {
-	const gamedata = readFileSync(resolve(__dirname, './default game 4.6.txt'), { encoding: 'utf-8' });
+test.each`
+	file         | version
+	${'default'} | ${'4.6'}
+	${'default'} | ${'5.1'}
+	${'default'} | ${'6.2'}
+	${'default'} | ${'7.1'}
+`('parses and serializes $file sample for bitsy v$version without modifying it', ({ file, version }) => {
+	const gamedata = readFileSync(resolve(__dirname, `./${file}_${version}.txt`), { encoding: 'utf-8' });
 	expect(BitsyParser.parse(gamedata.split('\n')).toString()).toBe(gamedata);
 });
