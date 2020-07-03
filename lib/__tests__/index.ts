abb30db3dd5444f27a3edec8dc29d96b9672bc9a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-07-02 23:34:42 -0400

    add "complex" 7.1 test
    includes more features than the default game

--- lib/__tests__/index.ts
+++ lib/__tests__/index.ts
@@ -8,6 +8,7 @@ test.each`
 	${'default'} | ${'5.1'}
 	${'default'} | ${'6.2'}
 	${'default'} | ${'7.1'}
+	${'complex'} | ${'7.1'}
 `('parses and serializes $file sample for bitsy v$version without modifying it', ({ file, version }) => {
 	const gamedata = readFileSync(resolve(__dirname, `./${file}_${version}.txt`), { encoding: 'utf-8' });
 	expect(BitsyParser.parse(gamedata.split('\n')).toString()).toBe(gamedata);
