a3ea9b762c7907a9454b93138887793390db5857
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-07-16 19:19:44 -0400

    add test for 8.0 gamedata using new features

--- lib/__tests__/index.ts
+++ lib/__tests__/index.ts
@@ -12,6 +12,7 @@ test.each`
 	${'default'} | ${'7.1'}
 	${'complex'} | ${'7.1' /*includes extended palette, extended animation frames, exit transitions, exit dialog */}
 	${'default'} | ${'8.0'}
+	${'complex'} | ${'8.0' /*includes sprite background colours, avatar by room */}
 	`('parses and serializes $file sample for bitsy v$version without modifying it', ({ file, version }) => {
 	const gamedata = readFileSync(resolve(__dirname, `./${file}_${version}.txt`), { encoding: 'utf-8' }).replace(/\r/g, '');
 	expect(BitsyParser.parse(gamedata.split('\n')).toString()).toBe(gamedata);
